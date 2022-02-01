6b44d41bc5b11cd9b3651b63447e0b51a9dfe3a6
Heitor <heitor.santos@quasar.srv.br>
2022-02-01 19:08:46 -0300

    Commit 15

--- Price.java
+++ Price.java
@@ -4,9 +4,6 @@ public abstract class Price {
     public abstract double getCharge(int daysRented);
 
     public int getFrequentRenterPoints(int daysRented) {
-        //Adicionar o trecho de código extraído.
-        if ((getPriceCode() == Movie.NEW_RELEASE) &&
-            daysRented > 1) return 2;
         return 1;
     }
 }
